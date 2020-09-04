0bb305602c6b1c75a006acbece5a21f71a3c2371
Gatoreno <erp.reno@hotmail.com>
2020-09-04 14:28:53 -0400

    added table view

---
+++ SimpleListApp/TableAdapter/TableViewSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using UIKit;
+
+namespace SimpleListApp.TableAdapter
+{
+    public class TableViewSource : UITableViewSource
+    {
+        List<string> tableItems;
+        public TableViewSource(List<string> _tableItems)
+        {
+
+            tableItems = _tableItems;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("tableViewCell");
+            cell.TextLabel.Text = tableItems[indexPath.Row];
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            // this tell our source how many rows we need now and in the future
+            return tableItems.Count;
+        }
+    }
+}
---
+++ SimpleListApp/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using Foundation;
+using SimpleListApp.TableAdapter;
+using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace SimpleListApp
+{
+    public partial class ViewController : UIViewController
+    {
+
+        List<string> tItems;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+            tItems = new List<string>();
+            for (int i = 0; i < 30; i++)
+            {
+                tItems.Add($"My i value: {i}");
+            }
+
+         }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            tableView.Source = new TableViewSource(tItems);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
